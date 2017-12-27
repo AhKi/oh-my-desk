962901a1e9d3e6eb19859bb6f2ee397df23e8303
ahnhyunmo <gusahdks@gmail.com>
2017-12-28 02:26:43 +0900

    Add react-router v4
    - must add react-router-redux after this commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import CounterContainers from 'containers/CounterContainer';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from 'routes';
 import store from './store';
 
 ReactDOM.render(
 	<Provider store={store}>
-		<CounterContainers />
+		<BrowserRouter>
+			<Routes />
+		</BrowserRouter>
 	</Provider>,
 	document.getElementById('root'),
 );
