ca8a3506f028c18bbe3c9be8d42a608c134796a6
suyoung <suyoung154@gmail.com>
2017-12-29 02:49:36 +0900

    remove window-all-closed event

--- main.js
+++ main.js
@@ -66,9 +66,6 @@ function createSetting() {
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
 })
 
 app.on('activate', () => {
