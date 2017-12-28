08b56e604f2a5aee147f0b16bbde78c84623f8d9
hyunmoahn <mos_dev@naver.com>
2017-12-29 01:10:08 +0900

    add production build webpack.config.env
    type script that 'yarn build:app'

---
+++ webpack.prod.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const webpackMerge = require('webpack-merge');
+const webpackBaseConfig = require('./webpack.basic.config');
+
+module.exports = webpackMerge(webpackBaseConfig, {
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+  },
+});
