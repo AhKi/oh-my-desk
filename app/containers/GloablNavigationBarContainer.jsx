89db9bf7adb4e4242cdaaf733c28c6f74f9d9a43
ahnhyunmo <gusahdks@gmail.com>
2017-12-30 15:45:24 +0900

    setting basic route
    - /setting
    - /widget-list
    - /widget-store

---
+++ app/containers/GloablNavigationBarContainer.jsx
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import GlobalNavigationBar from 'components/GlobalNavigationBar';
+
+const mapStateToProps = (state) => ({ // eslint-disable-line
+
+});
+
+const mapDispatchToProps = {
+
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GlobalNavigationBar);
