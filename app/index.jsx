4122e9ba7952f11d98f28c98e37fe3f19e7abcab
hyunmoahn <mos_dev@naver.com>
2017-12-30 13:43:14 +0900

    setting eslint env
    must check rules after
    current rules is minimum rule to develop

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { HashRouter  } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Routes from 'routes';
 import store from './store';
 
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
-import App from '../app';
 import CounterContainer from 'containers/CounterContainer';
 import RootPage from 'components/RootPage';
+import App from '../app';
 
 function routes() {
 	return (
