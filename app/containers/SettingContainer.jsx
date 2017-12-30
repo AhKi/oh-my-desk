a21f0c964dc9441a281c8ab0f18165ef484ece4d
ahnhyunmo <gusahdks@gmail.com>
2017-12-30 15:45:24 +0900

    setting basic route
    - /setting
    - /widget-list
    - /widget-store

---
+++ app/containers/SettingContainer.jsx
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import Setting from 'components/Setting';
+
+const mapStateToProps = state => ({ // eslint-disable-line
+
+});
+
+const mapDispatchToProps = {
+
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Setting);
