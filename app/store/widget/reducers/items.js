1e7dc9f7e709bb6cc6c2dbf5f38dee596579969c
ahnhyunmo <gusahdks@gmail.com>
2018-01-01 07:25:29 +0900

    add style of content in GNBWrapper

---
+++ app/store/widget/reducers/items.js
@@ -0,0 +1,12 @@
+import { handleActions } from 'redux-actions';
+import Immutable from 'immutable';
+import * as actions from '../actions';
+
+const initialState = Immutable.List();
+
+const itemsReducer = handleActions({
+	[actions.widgetListInfoStore]: (state, action) =>
+		Immutable.List(Object.keys(action.payload)),
+}, initialState);
+
+export default itemsReducer;
