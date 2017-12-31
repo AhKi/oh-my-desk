2779c18d7f78d94c8776435b001bb25f2d0ad59e
ahnhyunmo <gusahdks@gmail.com>
2018-01-01 00:47:20 +0900

    Add GNB bar

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router';
 import SettingContainer from 'containers/SettingContainer';
-import RootPage from 'components/RootPage';
 import WidgetList from 'components/WidgetList';
 import WidgetStore from 'components/WidgetStore';
+import GNBWrapper from 'components/GlobalNavigationBar/GNBWrapper';
 import App from '../app';
 
 function routes() {
 	return (
 		<App>
 			<Switch>
-				<Route exact path="/" component={RootPage} />
 				<Route path="/setting" component={SettingContainer} />
-				<Route path="/widget-list" component={WidgetList} />
-				<Route path="/widget-store" component={WidgetStore} />
+				<Route path="/widget-list" component={GNBWrapper(WidgetList)} />
+				<Route path="/widget-store" component={GNBWrapper(WidgetStore)} />
 				<Redirect to="/setting" from="/" />
 			</Switch>
 		</App>
