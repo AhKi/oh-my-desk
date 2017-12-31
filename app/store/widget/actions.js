661c6375d2f6563da116258d8a0bee2c0fa5d672
ahnhyunmo <gusahdks@gmail.com>
2018-01-01 07:58:35 +0900

    Add action, reducers, selector about selectedId

--- app/store/widget/actions.js
+++ app/store/widget/actions.js
@@ -1,5 +1,6 @@
 import { createAction } from 'redux-actions';
 import * as TYPES from '../actionTypes';
 
-export const widgetListInfoStore = createAction(TYPES.WIDGET_LIST_INFO_STORE); // eslint-disable-line
+export const widgetListInfoStore = createAction(TYPES.WIDGET_LIST_INFO_STORE);
+export const widgetListSelect = createAction(TYPES.WIDGET_LIST_SELECT);
 
