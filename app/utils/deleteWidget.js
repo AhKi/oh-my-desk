6195e570a5006c02a1b90eb2cbbb03b281071c8e
hyunmoahn <mos_dev@naver.com>
2018-01-01 23:48:10 +0900

    make widget delete

---
+++ app/utils/deleteWidget.js
@@ -0,0 +1,13 @@
+import * as IPC from 'constants/ipc';
+
+function deleteWidget(id) {
+	window.ipcRenderer.send(IPC.WIDGET_MANAGE, {
+		operation: 'DELETE',
+		widget: {
+			id,
+		},
+	});
+	window.ipcRenderer.send(IPC.WIDGET_INFO_REQUEST);
+}
+
+export default deleteWidget;
