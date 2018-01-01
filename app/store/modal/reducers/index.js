e2e9827abf093cf22bf3214a11d60957f4db599d
ahnhyunmo <gusahdks@gmail.com>
2018-01-01 15:30:24 +0900

    setting modal system and add make web widget modal

---
+++ app/store/modal/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux-immutable';
+import modalProps from './modalProps';
+import modalType from './modalType';
+
+const modalReducers = combineReducers({
+	modalProps,
+	modalType,
+});
+
+export default modalReducers;
---
+++ app/store/modal/reducers/modalType.js
@@ -0,0 +1,12 @@
+import { handleActions } from 'redux-actions';
+import * as actions from '../actions';
+
+const initialState = '';
+
+const modalTypeReducer = handleActions({
+	[actions.modalOpen]: (state, action) =>
+		action.payload.modalType,
+	[actions.modalClose]: () => initialState,
+}, initialState);
+
+export default modalTypeReducer;
