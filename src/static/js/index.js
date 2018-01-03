3b2d18863a39f153d0eef10fd16986f4dbac75a3
suyoung <suyoung154@gmail.com>
2018-01-04 05:22:33 +0900

    apply eslint to static/js

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,41 +1,38 @@
+const { remote } = require('electron');
+const { ipcRenderer } = require('electron');
 
-(function () {
-      
-  const remote = require('electron').remote; 
-  
-  function init() { 
-    document.getElementById("min-btn").addEventListener("click", function (e) {
-      const window = remote.getCurrentWindow();
-      window.minimize(); 
-    });
-    
-    document.getElementById("max-btn").addEventListener("click", function (e) {
-      const window = remote.getCurrentWindow();
-      if (!window.isMaximized()) {
-        window.maximize();
-      } else {
-        window.unmaximize();
-      }	 
-    });
-    
-    document.getElementById("close-btn").addEventListener("click", function (e) {
-      const window = remote.getCurrentWindow();
-      window.close();
-    }); 
-  }; 
-  
-  document.onreadystatechange = function () {
-    if (document.readyState == "complete") {
-      init(); 
-    }
-  };
+(function main() {
+	function init() {
+		document.getElementById('min-btn').addEventListener('click', () => {
+			const window = remote.getCurrentWindow();
+			window.minimize();
+		});
 
-  var ipcRenderer = require('electron').ipcRenderer;
+		document.getElementById('max-btn').addEventListener('click', () => {
+			const window = remote.getCurrentWindow();
+			if (!window.isMaximized()) {
+				window.maximize();
+			} else {
+				window.unmaximize();
+			}
+		});
 
-  ipcRenderer.on('widget-info', function (event, widget) {
-    document.getElementById('title').textContent = widget.name
-    document.getElementById('webview').loadURL(widget.url, {
-      userAgent: 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Mobile Safari/537.36'
-    })
-  });
-})();
+		document.getElementById('close-btn').addEventListener('click', () => {
+			const window = remote.getCurrentWindow();
+			window.close();
+		});
+	}
+
+	document.onreadystatechange = function change() {
+		if (document.readyState === 'complete') {
+			init();
+		}
+	};
+
+	ipcRenderer.on('widget-info', (event, widget) => {
+		document.getElementById('title').textContent = widget.name;
+		document.getElementById('webview').loadURL(widget.url, {
+			userAgent: 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Mobile Safari/537.36',
+		});
+	});
+}());
