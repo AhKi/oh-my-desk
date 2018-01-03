94222847cfa6e3383aba8ea730b5e2a1a84f86b9
suyoung <suyoung154@gmail.com>
2018-01-03 14:43:14 +0900

    accoding to eslint

--- src/Store.js
+++ src/Store.js
@@ -2,52 +2,58 @@ const electron = require('electron');
 const path = require('path');
 const fs = require('fs');
 
+function parseDataFile(filePath, defaults) {
+	let returnValue = defaults;
+
+	if (!fs.existsSync(path)) return defaults;
+
+	try {
+		returnValue = JSON.parse(fs.readFileSync(filePath));
+	} catch (error) {
+		throw new Error(error);
+	}
+
+	return returnValue;
+}
+
 class Store {
-  /**
- * @param {opts} 
+	/**
+ * @param { opts }
  * {
  *   configName: ... (the name of config file)
  *   defualts: ... (if there is no config file, it will be replace config file)
  * }
  */
-  constructor(opts) {
-    // renderer has to get `app` module via remote, main gets it directly
-    const userDataPath = (electron.app || electron.remote.app).getPath('userData');
-    this.path = path.join(userDataPath, opts.configName + '.json');
-    this.data = parseDataFile(this.path, opts.defaults);
-    this.save();
-  }
+	constructor(opts) {
+		// renderer has to get `app` module via remote, main gets it directly
+		const userDataPath = (electron.app || electron.remote.app).getPath('userData');
+		this.path = path.join(userDataPath, `${opts.configName}.json`);
+		this.data = parseDataFile(this.path, opts.defaults);
+		this.save();
+	}
 
-  get(key) {
-    return this.data[key];
-  }
+	get(key) {
+		return this.data[key];
+	}
 
-  set(key, val) {
-    this.data[key] = val;
-    this.save();
-  }
+	set(key, val) {
+		this.data[key] = val;
+		this.save();
+	}
 
-  delete(key) {
-    delete this.data[key];
-    this.save();
-  }
+	delete(key) {
+		delete this.data[key];
+		this.save();
+	}
 
-  getAll() {
-    return this.data;
-  }
+	getAll() {
+		return this.data;
+	}
 
-  save() {
-    fs.writeFileSync(this.path, JSON.stringify(this.data));
-  }
+	save() {
+		fs.writeFileSync(this.path, JSON.stringify(this.data));
+	}
 }
 
-function parseDataFile(filePath, defaults) {
-  try {
-    return JSON.parse(fs.readFileSync(filePath));
-  } catch(error) {
-    console.dir(error)
-    return defaults;
-  }
-}
 
 module.exports = Store;
--- src/WidgetManager.js
+++ src/WidgetManager.js
@@ -1,151 +1,168 @@
-const { BrowserWindow } = require('electron')
-const Store = require('./Store')
-const uuid = require('uuid/v4')
-const url = require('url')
-const path = require('path')
+const { BrowserWindow } = require('electron');
+const Store = require('./Store');
+const uuid = require('uuid/v4');
+const url = require('url');
+const path = require('path');
 
 class WidgetManager {
-  constructor() {
-    this.windows = [];
-    this.widgetStore = new Store({
-      configName: 'widgets',
-      defaults: []
-    })
+	constructor() {
+		this.windows = [];
+		this.widgetStore = new Store({ configName: 'widgets', defaults: {} });
 
-    // this is consumers of observer pattern
-    // createObserver will be passed widget information
-    this.createObserver = [this.openWindow.bind(this)];
-    this.updateObserver = [];
-    // deleteObserver will be passed widget id which be deleted
-    this.deleteObserver = [];
-  }
+		// this is consumers of observer pattern createObserver will be passed widget
+		// information
+		this.createObserver = [
+			this.openWindow.bind(this),
+		];
+		this.updateObserver = [];
+		// deleteObserver will be passed widget id which be deleted
+		this.deleteObserver = [];
+	}
 
-  create(widget) {
-    widget.id = uuid();
-    this.widgetStore.set(widget.id, widget)
+	create(_widget) {
+		const widget = _widget;
+		widget.id = uuid();
+		this.widgetStore.set(widget.id, widget);
 
-    this.createObserver.forEach((o) => o(widget))
-  }
+		this.createObserver.forEach((o) => { o(widget); });
+	}
 
-  update(widget) {
-    this.widgetStore.set(widget.id, widget)
-  }
+	update(widget) {
+		this.widgetStore.set(widget.id, widget);
+	}
 
-  delete(id) {
-    this.widgetStore.delete(id)
-    this.windows[id].close()
-    delete this.windows[id]
+	delete(id) {
+		this.widgetStore.delete(id);
+		if (this.windows[id]) {
+			this.windows[id].close();
+			delete this.windows[id];
+		}
 
-    this.deleteObserver.forEach((o) => o(id))
-  }
+		this.deleteObserver.forEach((o) => { o(id); });
+	}
 
-  getWidgets() {
-    return this.widgetStore.getAll();
-  }
+	getWidgets() {
+		return this.widgetStore.getAll();
+	}
 
-  openAllWindow() {
-    let widgets = this.widgetStore.getAll();
+	openAllWindow() {
+		const widgets = this.widgetStore.getAll();
+		const keys = Object.keys(widgets);
+		const values = Object.values(widgets);
 
-    for (let label in widgets) {
-      this.openWindow(widgets[label])
-    }
-  }
+		for (let i = 0; i < keys.length; i += 1) {
+			this.openWindow(values[i]);
+		}
+	}
 
-  openWindow(opt) {
-    if (!opt.isActive) return;
-    if (this.windows[opt.id]) {
-      this.windows[opt.id].focus();
-      return;
-    }
+	openWindow(opt) {
+		if (!opt.isActive) {
+			return;
+		}
 
-    let win = new BrowserWindow({
-        title: opt.name,
-        x: opt.position.x,
-        y: opt.position.y,
-        width: opt.size.width,
-        height: opt.size.height,
-        alwaysOnTop: opt.isOnTop,
-        autoHideMenuBar: true,
-        skipTaskbar: true,
-        show: false,
-        frame: false
-    })
-  
-    if (opt.type === 'web') {
-        // win.loadURL(opt.url, {userAgent: 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Mobile Safari/537.36'})
-      win.loadURL(url.format({
-        pathname: path.join(__dirname, 'static', 'index.html'),
-        protocol: 'file:',
-        slashes: true
-      }))
-    } else {
-        // some code creating window for native widget
-    }
+		if (this.windows[opt.id]) {
+			this.windows[opt.id].focus();
+			return;
+		}
 
-    win.webContents.on('did-finish-load', () => {
-      win.setTitle(opt.name)
-      win.webContents.send('widget-info', opt);
-    })
+		let win = new BrowserWindow({
+			title: opt.name,
+			x: opt.position.x,
+			y: opt.position.y,
+			width: opt.size.width,
+			height: opt.size.height,
+			alwaysOnTop: opt.isOnTop,
+			autoHideMenuBar: true,
+			skipTaskbar: true,
+			show: false,
+			frame: false,
+		});
 
-    win.once('ready-to-show', () => {
-      win.show()
-    })
-  
-    win.on('closed', () => {
-        win = null
-    })
+		if (opt.type === 'web') {
+			// win.loadURL(opt.url, {userAgent: 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5
+			// Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113
+			// Mobile Safari/537.36'})
+			win.loadURL(url.format({
+				pathname: path.join(__dirname, 'static', 'index.html'),
+				protocol: 'file:',
+				slashes: true,
+			}));
+		} else {
+			// some code creating window for native widget
+		}
 
-    win.on('move', (() => {
-      let position = win.getPosition();
+		win.webContents.on('did-finish-load', () => {
+			win.setTitle(opt.name);
+			win.webContents.send('widget-info', opt);
+		});
 
-      opt.position.x = position[0];
-      opt.position.y = position[1];
+		win.once('ready-to-show', () => {
+			win.show();
+		});
 
-      this.widgetStore.set(opt.id, opt);
-    }).bind(this))
-  
-    win.on('resize', (() => {
-      let size = win.getSize();
+		win.on('closed', () => {
+			win = null;
+		});
 
-      opt.size.width = size[0];
-      opt.size.height = size[1];
+		win.on('move', (() => {
+			const [position] = win.getPosition();
+			const _opt = opt;
 
-      this.widgetStore.set(opt.id, opt);
-    }).bind(this))
-  
-    this.windows[opt.id] = win
-  }
+			[_opt.position.x, _opt.position.y] = position;
 
-  onUpdateTray(callback) {
+			this.widgetStore.set(opt.id, opt);
+		}));
 
-    let funcWrapper = (function() {
-      callback(this.buildTrayContextMenuTemplate())
-    }).bind(this)
+		win.on('resize', (() => {
+			const size = win.getSize();
+			const _opt = opt;
 
-    funcWrapper();
+			[_opt.size.width, _opt.size.height] = size;
 
-    this.createObserver.push(funcWrapper)
-    this.deleteObserver.push(funcWrapper)
-  }
+			this
+				.widgetStore
+				.set(opt.id, opt);
+		}));
 
-  buildTrayContextMenuTemplate() {
-    let menuTemplate = [];
-    let widgets = this.getWidgets()
+		this.windows[opt.id] = win;
+	}
 
-    menuTemplate.push({label: 'Apps', type: 'normal'})
+	onUpdateTray(callback) {
+		function funcWrapper() {
+			callback(this.buildTrayContextMenuTemplate());
+		}
 
-    for (let label in widgets) {
-      let element = widgets[label];
+		funcWrapper.bind(this)();
 
-      menuTemplate.push({
-        label: element.name,
-        type: 'normal',
-        click: (() => { this.openWindow(element) }).bind(this)
-      })
-    }
+		this.createObserver.push(funcWrapper.bind(this));
+		this.deleteObserver.push(funcWrapper.bind(this));
+	}
 
-    return menuTemplate
-  }
+	buildTrayContextMenuTemplate() {
+		const menuTemplate = [];
+		const widgets = this.getWidgets();
+		const keys = Object.keys(widgets);
+		const values = Object.values(widgets);
+		let element = null;
+
+		menuTemplate.push({ label: 'Apps', type: 'normal' });
+
+		function openWindow(widget) {
+			this.openWindow(widget);
+		}
+
+		for (let i = 0; i < keys.length; i += 1) {
+			element = values[i];
+
+			menuTemplate.push({
+				label: element.name,
+				type: 'normal',
+				click: openWindow.bind(this, element),
+			});
+		}
+
+		return menuTemplate;
+	}
 }
 
-module.exports = WidgetManager
+module.exports = WidgetManager;
