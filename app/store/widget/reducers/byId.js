6cd68c915fc668d9955516851b3bed5c563da1bb
hyunmoahn <mos_dev@naver.com>
2018-01-07 21:07:51 +0900

    add action updateWidget

--- app/store/widget/reducers/byId.js
+++ app/store/widget/reducers/byId.js
@@ -7,6 +7,15 @@ const initialState = Immutable.Map();
 const byIdReducer = handleActions({
 	[actions.widgetListInfoStore]: (state, action) =>
 		Immutable.Map(action.payload).map(v => Immutable.Map(v)),
+	[actions.widgetInfoUpdate]: (state, action) => {
+		const { id, update } = action.payload;
+
+		return state.update(id, (v) => {
+			const obj = v.toObject();
+
+			return Immutable.Map(Object.assign({}, obj, update));
+		});
+	},
 }, initialState);
 
 export default byIdReducer;
