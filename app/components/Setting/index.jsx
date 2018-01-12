a44b54af7f1759681dd82be4e794e2a34ba4e8f6
ahnhyunmo <gusahdks@gmail.com>
2018-01-12 17:21:40 +0900

    Add temporary content in setting

--- app/components/Setting/index.jsx
+++ app/components/Setting/index.jsx
@@ -5,7 +5,10 @@ class Setting extends React.Component {
 	render() {
 		return (
 			<div className="Setting">
-				Setting
+				<div className="Setting__content">
+					<h4>설정</h4>
+					Setting
+				</div>
 			</div>
 		);
 	}
