f7d664f98c6467fbad205e9b6c6c6bbb8a6bf196
hyunmoAhn <mos_dev@naver.com>
2018-01-24 23:10:04 +0900

    make confirm modal component

---
+++ app/components/Modal/Content/Confirm/index.jsx
@@ -0,0 +1,10 @@
+import { connect } from 'react-redux';
+import { modalClose } from 'store/modal/actions';
+
+import Confirm from './Confirm';
+
+const mapDispatchToProps = {
+	onModalClose: modalClose,
+};
+
+export default connect(null, mapDispatchToProps)(Confirm);
