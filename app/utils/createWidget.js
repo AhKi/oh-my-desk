8fd87672424bb0caf5889fac4fbf2eaf8e9420a7
ahnhyunmo <gusahdks@gmail.com>
2018-01-25 10:06:23 +0900

    Hotfix make util function

--- app/utils/createWidget.js
+++ app/utils/createWidget.js
@@ -1,6 +1,7 @@
 import * as IPC from 'constants/ipc';
 
 function createWidget(type, info) {
+	console.log(info);
 	window.ipcRenderer.send(IPC.WIDGET_MANAGE, {
 		operation: 'CREATE',
 		widget: {
@@ -8,13 +9,13 @@ function createWidget(type, info) {
 			type,
 			name: info.name,
 			url: info.url,
-			position: info.position && {
-				x: info.position.x || 600,
-				y: info.position.y || 100,
+			position: {
+				x: (info.position && info.position.x) || 600,
+				y: (info.position && info.position.y) || 100,
 			},
-			size: info.size && {
-				width: info.size.width || 300,
-				height: info.size.height || 400,
+			size: {
+				width: (info.size && info.size.width) || 300,
+				height: (info.size && info.size.height) || 400,
 			},
 			transparency: info.transparency || 0.7,
 			isActive: info.isActive || true,
