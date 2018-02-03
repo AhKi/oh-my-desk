e4fdcf8b1a43a83c0d74cce90becc60f6613efa0
hyunmoAhn <mos_dev@naver.com>
2018-02-03 15:11:23 +0900

    add `src/` and `main.js` code in lint hook.

--- src/WidgetManager.js
+++ src/WidgetManager.js
@@ -1,9 +1,8 @@
-const { BrowserWindow, ipcMain } = require('electron');
+const { BrowserWindow } = require('electron');
 const Store = require('./Store');
 const uuid = require('uuid/v4');
 const url = require('url');
 const path = require('path');
-const setting_win = require('../main.js');
 
 class WidgetManager {
   constructor(option) {
