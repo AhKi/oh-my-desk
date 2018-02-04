d92c38620c1a5c00a3bb272e72c679a79cf21841
hyunmoAhn <mos_dev@naver.com>
2018-02-04 22:32:42 +0900

    split route about widget-list and widget-setting

---
+++ app/containers/WidgetListContainer.jsx
@@ -0,0 +1,34 @@
+import { connect } from 'react-redux';
+import WidgetList from 'components/WidgetList';
+import GNBWrapper from 'components/GlobalNavigationBar/GNBWrapper';
+import {
+  widgetListInfoStore,
+  widgetListSelect,
+  widgetInfoUpdateWithIPC,
+} from 'store/widget/actions';
+import {
+  modalOpen,
+  modalClose,
+} from 'store/modal/actions';
+
+import {
+  selectedIdSelector,
+  getSelectedWidget,
+  getWidgetInfo,
+} from 'store/widget/selectors';
+
+const mapStateToProps = state => ({
+  list: getWidgetInfo(state),
+  selectedId: selectedIdSelector(state),
+  selectedWidget: getSelectedWidget(state),
+});
+
+const mapDispatchToProps = {
+  onModalClose: modalClose,
+  onModalOpen: modalOpen,
+  onStoreWidgetInfo: widgetListInfoStore,
+  onSelectItem: widgetListSelect,
+  onUpdateInfoWithIPC: widgetInfoUpdateWithIPC,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GNBWrapper(WidgetList));
