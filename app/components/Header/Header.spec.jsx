2939442ee384f847552e3efe10daba63088cb13e
hyunmoAhn <mos_dev@naver.com>
2018-02-05 00:00:27 +0900

    add Header Component for page header

---
+++ app/components/Header/Header.spec.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Header from './';
+
+describe('<Header />', () => {
+  it('should match to snapshot when render default', () => {
+    const wrapper = shallow(<Header />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
