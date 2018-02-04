4ef705374c96af9af0d7b8c76086063a7fc75d51
hyunmoAhn <mos_dev@naver.com>
2018-02-05 01:41:15 +0900

    refactor WidgetListItem and type test code

--- app/components/WidgetList/components/WidgetListBox/index.jsx
+++ app/components/WidgetList/components/WidgetListBox/index.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ListItem from 'components/ListItem/index';
+import WidgetListItem from '../WidgetListItem/index';
 import './WidgetListBox.scss';
 
 const propTypes = {
   list: PropTypes.array, // eslint-disable-line
   selectedId: PropTypes.string,
   onSelectItem: PropTypes.func,
+  onUpdateInfoWithIPC: PropTypes.func,
+  onModalOpen: PropTypes.func,
 };
 
 const defaultProps = {
   list: [],
   selectedId: '',
   onSelectItem() {},
+  onUpdateInfoWithIPC() {},
+  onModalOpen() {},
 };
 
 class WidgetListBox extends React.Component {
   render() {
-    const { list, selectedId, onSelectItem } = this.props;
+    const {
+      list,
+      selectedId,
+      onSelectItem,
+      onUpdateInfoWithIPC,
+      onModalOpen,
+    } = this.props;
 
     return (
       <div className="WidgetListBox">
@@ -30,11 +40,13 @@ class WidgetListBox extends React.Component {
             <li className="WidgetListBox__title WidgetListBox__title-more">More</li>
           </ul>
           <ul>
-            {list.map(item => (<ListItem
+            {list.map(item => (<WidgetListItem
               key={item.id}
               item={item}
               selectedId={selectedId}
               onSelectItem={onSelectItem}
+              onUpdateInfoWithIPC={onUpdateInfoWithIPC}
+              onModalOpen={onModalOpen}
             />))}
           </ul>
         </div>
---
+++ app/components/WidgetList/components/WidgetListItem/index.jsx
@@ -0,0 +1,173 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import { Link } from 'react-router-dom';
+
+import ToggleButton from 'components/Button/ToggleButton';
+import OutsideClickHandler from 'components/OutsideClickHandler';
+import * as IPC from 'constants/ipc';
+import * as MODAL from 'constants/modal';
+import './WidgetListItem.scss';
+
+const propTypes = {
+  item: PropTypes.object, // eslint-disable-line
+  onSelectItem: PropTypes.func,
+  onUpdateInfoWithIPC: PropTypes.func,
+  onModalOpen: PropTypes.func,
+};
+
+const defaultProps = {
+  item: {},
+  onSelectItem() {},
+  onUpdateInfoWithIPC() {},
+  onModalOpen() {},
+};
+
+class WidgetListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActiveMore: false,
+    };
+    this.handleSelectList = this.handleSelectList.bind(this);
+    this.handleWidgetOpen = this.handleWidgetOpen.bind(this);
+    this.handleToggleIsActive = this.handleToggleIsActive.bind(this);
+    this.handleToggleAlwaysTop = this.handleToggleAlwaysTop.bind(this);
+    this.handleToggleMore = this.handleToggleMore.bind(this);
+    this.handleOpenDeleteModal = this.handleOpenDeleteModal.bind(this);
+  }
+
+  handleSelectList() {
+    window.ipcRenderer.send(IPC.WIDGET_SHOW_INACTIVE, this.props.item.id);
+    this.props.onSelectItem(this.props.item.id);
+  }
+
+  handleWidgetOpen() {
+    window.ipcRenderer.send(IPC.WIDGET_OPEN,
+      Object.assign({}, this.props.item, { isActive: true }));
+  }
+
+  handleToggleIsActive() {
+    const { item, onUpdateInfoWithIPC } = this.props;
+    const nextInfo = Object.assign({}, item, {
+      isActive: !item.isActive,
+    });
+    onUpdateInfoWithIPC(item.id, nextInfo);
+  }
+
+  handleToggleAlwaysTop() {
+    const { item, onUpdateInfoWithIPC } = this.props;
+    const nextInfo = Object.assign({}, item, {
+      isOnTop: !item.isOnTop,
+    });
+    onUpdateInfoWithIPC(item.id, nextInfo);
+  }
+
+  handleToggleMore(bool) {
+    const { isActiveMore } = this.state;
+
+    if (bool === undefined) {
+      this.setState({ isActiveMore: !isActiveMore });
+      return;
+    }
+
+    if (bool !== isActiveMore) {
+      this.setState({ isActiveMore: bool });
+    }
+  }
+
+  handleOpenDeleteModal() {
+    this.props.onModalOpen(MODAL.DELETE_CONFIRM_WIDGET, {
+      id: this.props.item.id,
+      name: this.props.item.name,
+    });
+  }
+
+  render() {
+    const { item } = this.props;
+    const { isActiveMore } = this.state;
+    const itemClassName = cx('WidgetListItem', 'WidgetListItem__list-box');
+
+    return (
+      <ul // eslint-disable-line
+        className={itemClassName}
+        onClick={this.handleSelectList}
+        onDoubleClick={this.handleWidgetOpen}
+      >
+        <li className="WidgetListItem__list WidgetListItem__list-name">
+          {item.name}
+        </li>
+        <li className="WidgetListItem__list">
+          <button
+            className="WidgetListItem__button"
+            type="button"
+            onClick={this.handleWidgetOpen}
+          >
+            {item.url}
+          </button>
+        </li>
+        <li className="WidgetListItem__list WidgetListItem__list-activated">
+          <ToggleButton
+            checkedValue={null}
+            unCheckedValue={null}
+            isCheck={item.isActive}
+            onToggle={this.handleToggleIsActive}
+          />
+        </li>
+        <li className="WidgetListItem__list WidgetListItem__list-always-top">
+          <button
+            className="WidgetListItem__button"
+            type="button"
+            onClick={this.handleToggleAlwaysTop}
+          >
+            {item.isOnTop ? 'pin up' : 'unpin'}
+          </button>
+        </li>
+        <li className="WidgetListItem__list WidgetListItem__more">
+          {isActiveMore ?
+            <OutsideClickHandler onOutSideClick={() => this.handleToggleMore(false)}>
+              <button
+                className="WidgetListItem__button"
+                data-name="outside-inner-more-btn"
+                type="button"
+                onClick={() => this.handleToggleMore()}
+              >
+                {'···'}
+              </button>
+              <ul className="WidgetListItem__more-menu">
+                <Link
+                  className="WidgetListItem__more-list"
+                  to="/widget-setting"
+                >
+                  edit <i className="fas fa-pencil-alt" />
+                </Link>
+                <li className="WidgetListItem__more-list">
+                  <button
+                    className="WidgetListItem__button"
+                    type="button"
+                    onClick={this.handleOpenDeleteModal}
+                  >
+                    remove <i className="fas fa-trash-alt" />
+                  </button>
+                </li>
+              </ul>
+            </OutsideClickHandler> :
+            <button
+              className="WidgetListItem__button"
+              data-name="more-btn"
+              type="button"
+              onClick={() => this.handleToggleMore()}
+            >
+              {'···'}
+            </button>
+          }
+        </li>
+      </ul>
+    );
+  }
+}
+
+WidgetListItem.propTypes = propTypes;
+WidgetListItem.defaultProps = defaultProps;
+
+export default WidgetListItem;
