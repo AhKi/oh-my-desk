bb5347074dfe44cc281d1400cc4d38bf810de1cd
hyunmoAhn <mos_dev@naver.com>
2018-02-04 17:01:00 +0900

    add ToggleButton TestCode.

---
+++ app/components/Button/ToggleButton/ToggleButton.spec.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import ToggleButton from './';
+
+describe('<ToggleButton />', () => {
+  it('should match to snapshot when render default', () => {
+    const wrapper = shallow(<ToggleButton isCheck />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match to snapshot when isCheck === false', () => {
+    const wrapper = shallow(<ToggleButton isCheck={false} />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
