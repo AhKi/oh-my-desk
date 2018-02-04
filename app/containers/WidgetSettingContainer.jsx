58fe2d88c1dae1c9acbe170610d71730e9dd43cc
hyunmoAhn <mos_dev@naver.com>
2018-02-04 22:32:42 +0900

    split route about widget-list and widget-setting

--- app/containers/WidgetSettingContainer.jsx
+++ app/containers/WidgetSettingContainer.jsx
@@ -1,34 +1,8 @@
 import { connect } from 'react-redux';
 import WidgetSetting from 'components/WidgetSetting';
 import GNBWrapper from 'components/GlobalNavigationBar/GNBWrapper';
-import {
-  widgetListInfoStore,
-  widgetListSelect,
-  widgetInfoUpdateWithIPC,
-} from 'store/widget/actions';
-import {
-  modalOpen,
-  modalClose,
-} from 'store/modal/actions';
 
-import {
-  selectedIdSelector,
-  getSelectedWidget,
-  getWidgetInfo,
-} from 'store/widget/selectors';
-
-const mapStateToProps = state => ({
-  list: getWidgetInfo(state),
-  selectedId: selectedIdSelector(state),
-  selectedWidget: getSelectedWidget(state),
-});
-
-const mapDispatchToProps = {
-  onModalClose: modalClose,
-  onModalOpen: modalOpen,
-  onStoreWidgetInfo: widgetListInfoStore,
-  onSelectItem: widgetListSelect,
-  onUpdateInfoWithIPC: widgetInfoUpdateWithIPC,
-};
+const mapStateToProps = () => ({});
+const mapDispatchToProps = {};
 
 export default connect(mapStateToProps, mapDispatchToProps)(GNBWrapper(WidgetSetting));
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router';
 import SettingContainer from 'containers/SettingContainer';
+import WidgetListContainer from 'containers/WidgetListContainer';
 import WidgetSettingContainer from 'containers/WidgetSettingContainer';
 import WidgetStore from 'components/WidgetStore';
 import GNBWrapper from 'components/GlobalNavigationBar/GNBWrapper';
@@ -10,9 +11,10 @@ function routes() {
   return (
     <App>
       <Switch>
-        <Route path="/setting" component={SettingContainer} />
-        <Route path="/widget-list" component={WidgetSettingContainer} />
+        <Route path="/widget-list" component={WidgetListContainer} />
+        <Route path="/widget-setting" component={WidgetSettingContainer} />
         <Route path="/widget-store" component={GNBWrapper(WidgetStore)} />
+        <Route path="/setting" component={SettingContainer} />
         <Redirect to="/widget-list" from="/" />
       </Switch>
     </App>
