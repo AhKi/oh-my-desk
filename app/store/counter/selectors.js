e2cca39fbaa66e0c1c1588d0144dcd060480169f
hyunmoAhn <mos_dev@naver.com>
2018-02-04 18:04:09 +0900

    delete unused code about sample counter

--- app/store/counter/selectors.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from 'reselect';
-
-export const counterSelector = state => state.get('counter');
-
-export const valueSelector = createSelector(
-  counterSelector,
-  counter => counter.get('value'),
-);
