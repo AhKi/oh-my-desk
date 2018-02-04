31a56b93e6c14b0a49434a3a8feedf18aff2be39
hyunmoAhn <mos_dev@naver.com>
2018-02-04 22:00:08 +0900

    delete unused code in reducer

--- app/store/rootReducer.js
+++ app/store/rootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux-immutable';
-import counter from './counter/reducers';
 import modal from './modal/reducers';
 import widget from './widget/reducers';
 
 const rootReducer = combineReducers({
-  counter,
   modal,
   widget,
 });
