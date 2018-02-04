64acda44df0a6634cf8c5792d59f4c57f81ce1bb
hyunmoAhn <mos_dev@naver.com>
2018-02-04 23:45:58 +0900

    refactor GlobalNavigationBar and write test code

--- app/components/GlobalNavigationBar/index.jsx
+++ app/components/GlobalNavigationBar/index.jsx
@@ -8,44 +8,82 @@ const propTypes = {
   match: PropTypes.shape({
     path: PropTypes.string,
   }),
+  onListClick: PropTypes.func,
+  onSettingClick: PropTypes.func,
+  onStoreClick: PropTypes.func,
 };
 
 const defaultProps = {
   match: {},
+  onListClick() {},
+  onSettingClick() {},
+  onStoreClick() {},
 };
 
-const listClassName = (current, target) => cx(
-  'list-group-item',
-  'list-group-item-action',
-  'GlobalNavigationBar__list',
-  {
-    active: current === target,
-  },
-);
-
 class GlobalNavigationBar extends React.Component {
+  static listClassName(current, target) {
+    let isActive;
+
+    if (typeof target === 'string') {
+      isActive = current === target;
+    } else if (Array.isArray(target)) {
+      isActive = target.indexOf(current) !== -1;
+    } else {
+      isActive = false;
+    }
+
+    return cx(
+      'GlobalNavigationBar__list',
+      {
+        'GlobalNavigationBar__list-active': isActive,
+      },
+    );
+  }
+
   render() {
     const menu = [
       {
-        path: '/setting',
-        name: 'Setting',
-      },
-      {
+        icon: '',
         path: '/widget-list',
         name: 'Widget',
+        match: ['/widget-list', '/widget-setting'],
+        onClick: this.props.onListClick,
       },
       {
+        icon: '',
+        path: '/setting',
+        name: 'Setting',
+        onClick: this.props.onSettingClick,
+      },
+      {
+        icon: '',
         path: '/widget-store',
         name: 'Widget store',
+        onClick: this.props.onStoreClick,
       },
     ];
 
     return (
       <div className="GlobalNavigationBar">
-        <ul className="list-group">
+        <div className="GlobalNavigationBar__title">
+          OhMyDesk
+        </div>
+        <ul className="GlobalNavigationBar__menu-box">
           {menu.map(v => (
-            <Link className="GlobalNavigationBar__link" to={v.path} key={v.path}>
-              <li className={listClassName(this.props.match.path, v.path)}>{v.name}</li>
+            <Link to={v.path} key={v.path}>
+              <li className={GlobalNavigationBar.listClassName(
+                this.props.match.path,
+                v.match || v.path,
+              )}
+              >
+                <button
+                  className="GlobalNavigationBar__list-btn"
+                  type="button"
+                  onClick={v.onClick}
+                >
+                  {v.name}
+                </button>
+              </li>
             </Link>
           ))}
         </ul>
