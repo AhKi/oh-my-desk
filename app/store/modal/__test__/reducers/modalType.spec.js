48d051066d3be009517c4311683b6f927721ff14
hyunmoAhn <mos_dev@naver.com>
2018-02-06 21:38:32 +0900

    write redux actions, reducers, selectors test code for store/modal

---
+++ app/store/modal/__test__/reducers/modalType.spec.js
@@ -0,0 +1,18 @@
+import modalType from 'store/modal/reducers/modalType';
+import * as actions from 'store/modal/actions';
+
+describe('test modal modalType reducer', () => {
+  it('should return initial state', () => {
+    expect(modalType(undefined, {})).toBe('');
+  });
+
+  it('should handle modalClose', () => {
+    expect(modalType(undefined, actions.modalClose())).toBe('');
+  });
+
+  it('should handle modalOpen', () => {
+    const payload = 'mock-type';
+
+    expect(modalType(undefined, actions.modalOpen(payload))).toBe('mock-type');
+  });
+});
