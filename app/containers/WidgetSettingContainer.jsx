8c50776d01aede351f65a61763d7312d076ac7b9
hyunmoAhn <mos_dev@naver.com>
2018-02-06 18:04:44 +0900

    add WidgetSetting component and write test code

--- app/containers/WidgetSettingContainer.jsx
+++ app/containers/WidgetSettingContainer.jsx
@@ -1,8 +1,16 @@
 import { connect } from 'react-redux';
 import WidgetSetting from 'components/WidgetSetting';
 import GNBWrapper from 'components/GlobalNavigationBar/GNBWrapper';
+import { getSelectedWidget } from 'store/widget/selectors';
+import { widgetInfoUpdateWithIPC } from 'store/widget/actions';
+import { modalOpen } from 'store/modal/actions';
 
-const mapStateToProps = () => ({});
-const mapDispatchToProps = {};
+const mapStateToProps = state => ({
+  item: getSelectedWidget(state),
+});
+const mapDispatchToProps = {
+  onModalOpen: modalOpen,
+  onStoreWidgetInfo: widgetInfoUpdateWithIPC,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(GNBWrapper(WidgetSetting));
