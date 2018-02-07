3873015c71168224fa1863a06b16e35ad51b1c12
hyunmoAhn <mos_dev@naver.com>
2018-02-08 01:04:32 +0900

    extract number per page to constants

---
+++ app/constants/index.js
@@ -0,0 +1,1 @@
+export const NUMBER_PER_PAGE = 6; // eslint-disable-line
--- app/store/widget/reducers/maxPage.js
+++ app/store/widget/reducers/maxPage.js
@@ -1,12 +1,12 @@
 import { handleActions } from 'redux-actions';
+import * as CONST from 'constants/index';
 import * as actions from '../actions';
 
-const NUMBER_PER_PAGE = 6;
 const initialState = 1;
 
 const maxPageReducer = handleActions({
   [actions.widgetListInfoStore]: (state, action) =>
-    Math.ceil(Object.keys(action.payload).length / NUMBER_PER_PAGE),
+    Math.ceil(Object.keys(action.payload).length / CONST.NUMBER_PER_PAGE),
 }, initialState);
 
 export default maxPageReducer;
