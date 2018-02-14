e74dc59facd25871db1e852f7950847cca711d0f
hyunmoAhn <mos_dev@naver.com>
2018-02-14 22:15:17 +0900

    add widget page

---
+++ app/widget.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WebWidget from 'components/WebWidget';
+import './style.scss';
+
+ReactDOM.render(
+  <WebWidget />,
+  document.getElementById('root'),
+);
