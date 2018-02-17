057008b671bd63a91f0303fc0c1ed393ad8d5b8d
hyunmoAhn <mos_dev@naver.com>
2018-02-17 23:26:55 +0900

    add config code for using file-loader file in dev environment

--- webpack.basic.config.js
+++ webpack.basic.config.js
@@ -45,6 +45,8 @@ module.exports = {
         loader: 'file-loader',
         options: {
           name: '[path][name].[ext]',
+          publicPath: process.env.NODE_ENV === 'development' ? path.join(__dirname) : undefined,
+          outputPath: process.env.NODE_ENV === 'development' ? '/' : undefined,
         },
       }
     ]
