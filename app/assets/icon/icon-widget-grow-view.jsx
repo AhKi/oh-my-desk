0a3333a4a0a3ba9f4c50af18fff03a9620325dd7
ginna <ginna9498@gmail.com>
2018-02-25 22:50:31 +0900

    add svg icons and jsx sorces

---
+++ app/assets/icon/icon-widget-grow-view.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import React from 'react';
+
+const WidgetGrowIcon = (props) => (
+  <svg className={props.className} width="15px" height="15px" viewBox="0 0 15 15" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+    <g id="Artboard-Copy" stroke="none" stroke-Width="1" fill="none" fillRule="evenodd" transform="translate(-321.000000, -97.000000)">
+      <path d="M332.19244,99.7130756 L330.438542,97.9591782 L335.33561,97.7014378 L335.07787,102.598506 L333.223402,100.744037 L330.326012,103.641427 C330.041319,103.926119 329.579742,103.926119 329.29505,103.641427 C329.010358,103.356735 329.010358,102.895158 329.29505,102.610466 L332.19244,99.7130756 Z" id="Combined-Shape-Copy-3" fill="#BEC0CC" fillRule="nonzero"></path>
+      <path d="M325.057976,106.87855 L323.304078,105.124653 L328.201146,104.866912 L327.943406,109.76398 L326.088937,107.909512 L323.191547,110.806902 C322.906855,111.091594 322.445278,111.091594 322.160586,110.806902 C321.875893,110.52221 321.875893,110.060632 322.160586,109.77594 L325.057976,106.87855 Z" id="Combined-Shape-Copy-5" fill="#BEC0CC" fillRule="nonzero" transform="translate(325.074106, 107.943667) rotate(-180.000000) translate(-325.074106, -107.943667) "></path>
+    </g>
+  </svg>
+);
+
+export default WidgetGrowIcon;
