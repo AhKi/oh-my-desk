71ccfa3e288cdf634af506267448af5517b182d9
hyunmoahn <mos_dev@naver.com>
2018-03-06 22:16:14 +0900

    add Test Code about DeleteConfirmWidget Component

---
+++ app/page/setting/components/Modal/Content/DeleteConfirmWidget/DeleteConfirmWidget.spec.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import * as utils from 'utils/deleteWidget';
+import DeleteConfirmWidget from './DeleteConfirmWidget';
+
+describe('<DeleteConfirmWidget />', () => {
+  it('should match to snapshot when render default', () => {
+    const wrapper = shallow(<DeleteConfirmWidget />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should call handleDeletWidget', () => {
+    const ipcRenderer = {
+      send() {},
+    };
+    window.ipcRenderer = ipcRenderer;
+    const deleteWidget = jest.spyOn(utils, 'default');
+    const onClose = jest.fn();
+    const wrapper = shallow(
+      <DeleteConfirmWidget
+        id="mock-id"
+        onClose={onClose}
+      />,
+    );
+
+    wrapper.instance().handleDeleteWidget();
+
+    expect(deleteWidget).toHaveBeenCalledTimes(1);
+    expect(deleteWidget).toHaveBeenCalledWith('mock-id');
+    expect(onClose).toHaveBeenCalledTimes(1);
+    expect(onClose).toHaveBeenCalledWith();
+  });
+});
