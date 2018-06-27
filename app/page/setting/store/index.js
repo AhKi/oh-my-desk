89a925fe4aca0c51154ceafd0eb3a5f11f9f5667
hyunmoahn <mos_dev@naver.com>
2018-06-28 03:55:08 +0900

    Update redux side library
    postpone redux update because breaking change exist v3 to v4

--- app/page/setting/store/index.js
+++ app/page/setting/store/index.js
@@ -1,13 +1,12 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './rootReducer';
 
 const middleWares = [thunk];
-const composeEnhancers = __DEV__ ?
-  (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose) : compose;
 const store = createStore(
   rootReducer,
-  composeEnhancers(
+  composeWithDevTools(
     applyMiddleware(...middleWares),
   ),
 );
