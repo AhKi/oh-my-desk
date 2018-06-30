0263536b894a54882cacd55430790b5be4a7649c
hyunmoahn <mos_dev@naver.com>
2018-07-01 01:51:13 +0900

    Delete WidgetStore page
    store will not make so delete code

--- app/page/setting/routes/index.jsx
+++ app/page/setting/routes/index.jsx
@@ -3,8 +3,6 @@ import { Switch, Route, Redirect } from 'react-router';
 import SettingContainer from 'setting/containers/SettingContainer';
 import WidgetListContainer from 'setting/containers/WidgetListContainer';
 import WidgetSettingContainer from 'setting/containers/WidgetSettingContainer';
-import WidgetStore from 'setting/components/WidgetStore';
-import GNBWrapper from 'setting/components/GlobalNavigationBar/GNBWrapper';
 import App from '../app';
 
 function routes() {
@@ -13,7 +11,6 @@ function routes() {
       <Switch>
         <Route path="/widget-list" component={WidgetListContainer} />
         <Route path="/widget-setting" component={WidgetSettingContainer} />
-        <Route path="/widget-store" component={GNBWrapper(WidgetStore)} />
         <Route path="/setting" component={SettingContainer} />
         <Redirect to="/widget-list" from="/" />
       </Switch>
