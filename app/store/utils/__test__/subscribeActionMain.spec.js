ed57933677bc7d759cb68ba9e42c2429fa28c4e7
hyunmoahn <mos_dev@naver.com>
2018-07-01 01:49:30 +0900

    Setting Controller in main process
    show issue #116(https://github.com/AhKi/oh-my-desk/issues/116)

--- app/store/utils/__test__/subscribeActionMain.spec.js
+++ app/store/utils/__test__/subscribeActionMain.spec.js
@@ -1,23 +1,39 @@
 import { ipcMain } from 'electron';
+import * as controller from 'process/main/controllers';
 import subscribeActionMain from '../subscribeActionMain';
 
 describe('test subscribeActionMain', () => {
-  it('should call ipcMain.on', () => {
+  describe('should call ipcMain.on', () => {
     const store = {
       dispatch: jest.fn(),
+      getState: jest.fn(),
     };
+    const initialState = { mock: 'initial' };
+    const nextState = { mock: 'next' };
     const payload = 'mock-payload';
+    controller.default = jest.fn();
+
+    store.getState.mockReturnValueOnce(initialState);
+    store.getState.mockReturnValueOnce(nextState);
 
     subscribeActionMain(store);
 
-    expect(ipcMain.on).toHaveBeenCalledTimes(1);
-    expect(ipcMain.on).toHaveBeenCalledWith('redux-action', expect.any(Function));
+    it('should call ipcMain correctly', () => {
+      expect(ipcMain.on).toHaveBeenCalledTimes(1);
+      expect(ipcMain.on).toHaveBeenCalledWith('redux-action', expect.any(Function));
+    });
 
-    const cb = ipcMain.on.mock.calls[0][1];
-    cb('someEvent', payload);
+    it('should call ipcMain callback correctly', () => {
+      const cb = ipcMain.on.mock.calls[0][1];
+      cb('someEvent', payload);
 
-    expect(store.dispatch).toHaveBeenCalledTimes(1);
-    expect(store.dispatch).toHaveBeenCalledWith(payload);
+
+      expect(store.dispatch).toHaveBeenCalledTimes(1);
+      expect(store.dispatch).toHaveBeenCalledWith(payload);
+
+      expect(controller.default).toHaveBeenCalledTimes(1);
+      expect(controller.default).toHaveBeenCalledWith(initialState, nextState);
+    });
   });
 
   it('should match global.getReduxState', () => {
--- app/store/utils/subscribeActionMain.js
+++ app/store/utils/subscribeActionMain.js
@@ -1,9 +1,14 @@
 import { ipcMain } from 'electron';
+import controller from 'process/main/controllers';
 
 export default function subscribeActionMain(store) {
   global.getReduxState = () => JSON.stringify(store.getState());
 
   ipcMain.on('redux-action', (e, payload) => {
+    const prevState = store.getState();
     store.dispatch(payload);
+    const nextState = store.getState();
+
+    controller(prevState, nextState);
   });
 }
