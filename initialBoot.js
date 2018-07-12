a412dbfb1a6f4909aff7647f085182df0898dbd3
hyunmoahn <mos_dev@naver.com>
2018-07-13 01:14:22 +0900

    Edit basic config about develop mode

--- initialBoot.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-require('babel-register');
-require('module-alias/register');
-
-const mainPath = path.join(__dirname, 'app/main');
-require(mainPath); // eslint-disable-line
