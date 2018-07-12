5d5de8b23c23c6dd4400534f1f4cf0bba1f2cde1
hyunmoahn <mos_dev@naver.com>
2018-07-13 00:59:13 +0900

    Edit path about config store

--- app/constants/path/index.js
+++ app/constants/path/index.js
@@ -2,8 +2,8 @@ import { app, remote } from 'electron';
 import path from 'path';
 
 export const ROOT_PATH = path.resolve(__dirname, '../../..');
-export const CONFIG_PATH = (app || remote.app).getPath('userData');
-export const SETTING_FILE_NAME = 'store.json';
+export const CONFIG_PATH = (app || remote.app).getPath('appData');
+export const SETTING_FILE_NAME = '/oh-my-desk/store.json';
 
 export const BUILD_PATH = process.env.NODE_ENV === 'production' ? 'build' : 'app/page';
 export const WIDGET_PATH = `${BUILD_PATH}/webview/widget.html`;
