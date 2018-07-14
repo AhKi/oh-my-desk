b5c7d46efed16d2af6d965da2583e9522f7bf12e
hyunmoahn <mos_dev@naver.com>
2018-07-14 13:53:02 +0900

    Attach basic tray menu page

---
+++ app/utils/process/trayMenuBar.js
@@ -0,0 +1,29 @@
+import { app, globalShortcut } from 'electron';
+import menuBar from 'menubar';
+import url from 'url';
+import * as PATH from 'constants/path';
+
+const trayMenuBar = menuBar({
+  icon: PATH.TRAY_ICON_PATH,
+  index: url.format({
+    pathname: PATH.SEARCH_PATH,
+    protocol: 'file:',
+    slashes: true,
+  }),
+  showDockIcon: true,
+  tooltip: `oh-my-desk ${app.getVersion()}`,
+});
+
+trayMenuBar.on('show', () => {
+  globalShortcut.register('Escape', () => {
+    if (trayMenuBar.window && trayMenuBar.window.isFocused()) {
+      trayMenuBar.hideWindow();
+    }
+  });
+});
+
+trayMenuBar.on('hide', () => {
+  globalShortcut.unregister('Escape');
+});
+
+export default trayMenuBar;
