0c2febcd8ac3eda80b475305d3578a0bff47507b
hyunmoahn <mos_dev@naver.com>
2018-07-18 04:30:50 +0900

    Setting about AutoLaunch when user login window

--- app/store/utils/controllers/preference/preference.spec.js
+++ app/store/utils/controllers/preference/preference.spec.js
@@ -1,5 +1,6 @@
 import * as TYPES from 'actions/actionTypes';
 import * as openPreference from 'utils/process/openPreference';
+import * as autoLaunch from 'utils/autoLaunch';
 import preferenceController from '.';
 
 describe('test preference controller', () => {
@@ -14,5 +15,18 @@ describe('test preference controller', () => {
 
     expect(mockOpenPreference).toHaveBeenCalledTimes(1);
     expect(mockOpenPreference).toHaveBeenCalledWith();
+  });
+
+  it('should handle TOGGLE_AUTO_LAUNCH', () => {
+    const mockAutoLaunch = jest.spyOn(autoLaunch, 'default');
+    mockAutoLaunch.mockImplementation(jest.fn());
+    const mockAction = {
+      type: TYPES.TOGGLE_AUTO_LAUNCH,
+    };
+
+    preferenceController(mockAction);
+
+    expect(mockAutoLaunch).toHaveBeenCalledTimes(1);
+    expect(mockAutoLaunch).toHaveBeenCalledWith();
   });
 });
