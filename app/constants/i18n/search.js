385d2ad79f4aa11a5822f103ab3fb6ef1bb39ff3
hyunmoahn <mos_dev@naver.com>
2018-07-22 21:24:16 +0900

    Default Search Menu View

---
+++ app/constants/i18n/search.js
@@ -0,0 +1,18 @@
+export default {
+  en: {
+    all: 'All',
+    bookMarkSearch: 'Search in "Favorites"',
+    favorites: 'Favorites',
+    newWidget: 'New Widget',
+    setting: 'Setting',
+    search: 'Search widgets',
+  },
+  ko: {
+    all: '전체 목록',
+    bookMarkSearch: '"즐겨찾기"에서 검색하기',
+    favorites: '즐겨찾기',
+    newWidget: '새 위젯',
+    setting: '설정',
+    search: '위젯 검색하기',
+  },
+};
--- app/page/search/index.jsx
+++ app/page/search/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
 import createStore from 'store/createStore';
 import subscribeActionRenderer from 'store/utils/subscribeActionRenderer';
+import 'scss/index.scss';
 import Search from './components/Search';
 
 const store = createStore();
@@ -11,9 +11,7 @@ subscribeActionRenderer(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter>
-      <Search />
-    </HashRouter>
+    <Search />
   </Provider>,
   document.getElementById('root'),
 );
