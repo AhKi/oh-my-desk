f362ebd2c7950e623abd37c78fad90449199e084
hyunmoahn <mos_dev@naver.com>
2018-07-26 19:36:43 +0900

    Close SearchWindow that doesn't wait focus out when 'enter' event

--- app/store/utils/controllers/index.js
+++ app/store/utils/controllers/index.js
@@ -1,9 +1,11 @@
 import rootController from './rootController';
 import preferenceController from './preference';
+import searchController from './search';
 import widgetController from './widget';
 
 const controller = (action, prevState, nextState) => rootController(action, prevState, nextState, {
   preferenceController,
+  searchController,
   widgetController,
 });
 
