dfe90997d59ffb25f1ec60565bf79c751d30835d
hyunmoahn <mos_dev@naver.com>
2018-07-29 22:14:56 +0900

    Default Basic Setting button position

--- app/page/preference/components/Setting/index.jsx
+++ app/page/preference/components/Setting/index.jsx
@@ -2,25 +2,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import i18n from 'constants/i18n';
 import ToggleButton from 'page/Components/ToggleButton';
+import './Setting.scss';
 
 const propTypes = {
+  lang: PropTypes.string,
   isAutoLaunch: PropTypes.bool,
   widgetMode: PropTypes.string,
+  onSetLanguageEnglish: PropTypes.func,
+  onSetLanguageKorean: PropTypes.func,
   onToggleAutoLaunch: PropTypes.func,
   onToggleWidgetMode: PropTypes.func,
 };
 
 const defaultProps = {
+  lang: 'English',
   isAutoLaunch: true,
   widgetMode: 'DESKTOP',
+  onSetLanguageEnglish() {},
+  onSetLanguageKorean() {},
   onToggleAutoLaunch() {},
   onToggleWidgetMode() {},
 };
 
 class Setting extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChangeLanguage = this.handleChangeLanguage.bind(this);
+  }
+
+  handleChangeLanguage(e) {
+    const language = e.target.value;
+    const { onSetLanguageEnglish, onSetLanguageKorean } = this.props;
+
+    if (language === 'English') {
+      onSetLanguageEnglish();
+    } else {
+      onSetLanguageKorean();
+    }
+  }
+
   render() {
     const text = i18n().preference;
     const {
+      lang,
       isAutoLaunch,
       widgetMode,
       onToggleAutoLaunch,
@@ -28,17 +52,47 @@ class Setting extends React.Component {
     } = this.props;
 
     return (
-      <div>
-        <p>{text.basicSetting}</p>
-        <ToggleButton
-          isCheck={isAutoLaunch}
-          onToggle={onToggleAutoLaunch}
-        />
-        <span>{text.autoStart}</span>
-        <select value={widgetMode} onChange={onToggleWidgetMode}>
-          <option value="DESKTOP">{text.desktopMode}</option>
-          <option value="MOBILE">{text.mobileMode}</option>
-        </select>
+      <div className="Setting">
+        <p className="Setting__title">{text.user}</p>
+        <ul className="Setting__list-set">
+          <li className="Setting__list">
+            {text.autoStart}
+            <ToggleButton
+              isCheck={isAutoLaunch}
+              onToggle={onToggleAutoLaunch}
+            />
+          </li>
+          <li className="Setting__list">
+            {text.autoWidgetActive}
+            <ToggleButton
+              isCheck={isAutoLaunch}
+              onToggle={onToggleAutoLaunch}
+            />
+          </li>
+          <li className="Setting__list">
+            {text.language}
+            <select value={lang} onChange={this.handleChangeLanguage}>
+              <option value="English">English</option>
+              <option value="Korean">한국어</option>
+            </select>
+          </li>
+        </ul>
+        <p className="Setting__title">{text.shape}</p>
+        <ul className="Setting__list-set">
+          <li className="Setting__list">
+            {text.defaultWidgetMode}
+            <select value={widgetMode} onChange={onToggleWidgetMode}>
+              <option value="DESKTOP">{text.desktopMode}</option>
+              <option value="MOBILE">{text.mobileMode}</option>
+            </select>
+          </li>
+          <li className="Setting__list">
+            {text.themeColor}
+            <select>
+              <option value="English">{text.colorSpaceGray}</option>
+            </select>
+          </li>
+        </ul>
       </div>
     );
   }
--- app/page/preference/containers/SettingContainer.jsx
+++ app/page/preference/containers/SettingContainer.jsx
@@ -1,21 +1,29 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import toJS from 'utils/toJS';
 import {
+  setLanguageEnglish,
+  setLanguageKorean,
   toggleAutoLaunch,
   toggleWidgetMode,
 } from 'actions/status';
 import {
   autoLaunchSelector,
+  langSelector,
   widgetModeSelector,
 } from 'store/share/status/selectors';
 import Setting from '../components/Setting';
 
 const mapStateToProps = state => ({
+  lang: langSelector(state),
   isAutoLaunch: autoLaunchSelector(state),
   widgetMode: widgetModeSelector(state),
 });
 const mapDispatchToProps = {
+  onSetLanguageEnglish: setLanguageEnglish,
+  onSetLanguageKorean: setLanguageKorean,
   onToggleAutoLaunch: toggleAutoLaunch,
   onToggleWidgetMode: toggleWidgetMode,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Setting);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(toJS(Setting)));
