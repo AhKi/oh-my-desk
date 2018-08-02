62033f68521d140f8a6b35b51045a9aacac5091d
hyunmoahn <mos_dev@naver.com>
2018-08-02 16:46:13 +0900

    Controller subscribe action that dispatched only main process.
    ## About
    
    Previous Controller is located in IPC communication logic,
    So controller subscribe action about only renderer process to main process.
    
    It is improved that controller subscribe every action whaterver main to main or renderer to renderer.
    
    ## Issue
    When improve Controller, We have global import module issue.
    Some local module like `components/i18n/index.js` and `store/createStore` is using both process that main and renderer.
    It doesn't matter because we write a code conditionally if it is used in main process or renderer process.
    But globally `import` make error. For example, `store/storeMain.js` must use only main process. But It imported in renderer process although not using.
    That make error, not using but import!,
    So we solve that conditionally import using `require` in ES5.

--- app/store/utils/subscribeActionMain.js
+++ app/store/utils/subscribeActionMain.js
@@ -1,14 +1,9 @@
 import { ipcMain } from 'electron';
-import controller from 'store/utils/controllers';
 
 export default function subscribeActionMain(store) {
   global.getReduxState = () => JSON.stringify(store.getState().get('share'));
 
   ipcMain.on('redux-action', (e, action) => {
-    const prevState = store.getState();
     store.dispatch(action);
-    const nextState = store.getState();
-
-    controller(action, prevState, nextState);
   });
 }
