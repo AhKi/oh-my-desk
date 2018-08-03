cf3729728ec64da5c4143f2c485f8cc00a03a727
hyunmoahn <mos_dev@naver.com>
2018-08-03 17:07:20 +0900

    `utils/process` function unit test code

--- __mocks__/electron-updater.js
+++ __mocks__/electron-updater.js
@@ -3,4 +3,6 @@ export default jest.fn();
 export const autoUpdater = {
   downloadUpdate: jest.fn(() => new Promise(() => {})),
   checkForUpdates: jest.fn(() => new Promise(() => {})),
+  on: jest.fn(),
+  setFeedURL: jest.fn(),
 };
