380d20b06ab74f9122bae7f0ef75bba25adc0448
hyunmoahn <mos_dev@naver.com>
2018-08-11 05:30:02 +0900

    Attach Logic about address-bar in widget
    - add browser movement
    - add that go to home url
    - add go forward
    - add go back
    - add reload
    - add that toggle isOnTop

--- app/page/webview/containers/WidgetHeaderContainer.jsx
+++ app/page/webview/containers/WidgetHeaderContainer.jsx
@@ -1,8 +1,11 @@
 import { connect } from 'react-redux';
+import { updateTargetWidgetInfo } from 'actions/widget';
 import WidgetHeader from '../components/WidgetHeader';
 
 const mapStateToProps = () => ({});
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = {
+  onUpdateWidgetInfo: updateTargetWidgetInfo,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(WidgetHeader);
