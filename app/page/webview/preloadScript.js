e01ae23cab4d49ffd841c4b6215e4441a0f10f37
hyunmoahn <mos_dev@naver.com>
2018-08-13 03:10:45 +0900

    web page reload event occur in globally

--- app/page/webview/preloadScript.js
+++ app/page/webview/preloadScript.js
@@ -6,16 +6,3 @@ Mousetrap.bind('command+left', () => {
 Mousetrap.bind('command+right', () => {
   window.history.forward();
 });
-
-document.addEventListener('keydown', (e) => {
-  let command;
-  if (process.platform === 'darwin') {
-    command = e.metaKey;
-  } else {
-    command = e.altKey;
-  }
-
-  if (command && e.key === 'r') {
-    window.location.reload();
-  }
-});
