bb603461644b658986f2e8dadb1898fee4e4f32a
hyunmoahn <mos_dev@naver.com>
2018-08-27 20:02:24 +0900

    Can open redux-devtools about store of main process.
    when start dev mode, use `yarn start-devtools` instead of `yarn start`
    Then open window of redux-devtools of main process.

--- app/utils/process/init.js
+++ app/utils/process/init.js
@@ -1,4 +1,9 @@
-import { app, globalShortcut } from 'electron';
+import {
+  app,
+  BrowserView,
+  BrowserWindow,
+  globalShortcut,
+} from 'electron';
 import autoLaunch from 'utils/autoLaunch';
 import autoUpdateConfig from 'utils/process/update/autoUpdateConfig';
 import createMenu from 'utils/process/createMenu';
@@ -32,6 +37,20 @@ function init() {
       TrayMenuBar.hideWindow();
     }
   });
+
+  if (process.env.IS_DEVTOOLS) {
+    const devToolsWin = new BrowserWindow({ title: 'main process redux-devtools', width: 500, height: 500 });
+    const devToolsView = new BrowserView();
+    devToolsWin.setBrowserView(devToolsView);
+    devToolsView.setBounds({
+      x: 0,
+      y: 0,
+      width: 500,
+      height: 480,
+    });
+    devToolsView.setAutoResize({ width: true, height: true });
+    devToolsView.webContents.loadURL('http://remotedev.io/local/');
+  }
 
   store.dispatch(setInitialStore());
 }
