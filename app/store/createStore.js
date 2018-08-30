36345789b58133b929ddc662c0dadb1bb9253563
hyunmoahn <mos_dev@naver.com>
2018-08-30 18:35:29 +0900

    Fix Bug about packaging
    remote-redux-devtools occur error when packaging electron.
    So It is used branch import about env.IS_DEVTOOLS.

--- app/store/createStore.js
+++ app/store/createStore.js
@@ -2,11 +2,9 @@ import { createStore, applyMiddleware } from 'redux';
 import { remote } from 'electron';
 import Immutable from 'immutable';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { composeWithDevTools as remoteComposeWithDevTools } from 'remote-redux-devtools';
 import getStoredDataInDisk from 'utils/getStoredDataInDisk';
 import rootReducer from './rootReducer';
 import categorizeActionInRenderer from './utils/middleware/categorizeActionInRenderer';
-
 
 const MAIN = 'MAIN';
 const store = (scope) => {
@@ -16,7 +14,8 @@ const store = (scope) => {
       require('./utils/middleware/categorizeActionInMain').default; // eslint-disable-line global-require
     const storedData = getStoredDataInDisk();
     const middleware = applyMiddleware(categorizeActionInMain);
-    const attachDevToolsWithMiddleware = process.env.NODE_ENV === 'development' ? remoteComposeWithDevTools({ name: 'oh-my-desk main process' })(middleware) : middleware;
+    const attachDevToolsWithMiddleware = process.env.IS_DEVTOOLS ?
+      require('remote-redux-devtools').composeWithDevTools({ name: 'oh-my-desk main process' })(middleware) : middleware; // eslint-disable-line global-require
 
     return createStore(
       rootReducer,
