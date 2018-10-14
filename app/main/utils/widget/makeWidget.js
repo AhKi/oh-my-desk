910b9ee5adb6f54cd6f4a75f7c2db138a68d4990
hyunmoahn <mos_dev@naver.com>
2018-10-14 23:40:11 +0900

    Confirm when close about making and editing widget.

--- app/main/utils/widget/makeWidget.js
+++ app/main/utils/widget/makeWidget.js
@@ -1,10 +1,12 @@
-import { BrowserWindow } from 'electron';
+import { BrowserWindow, dialog } from 'electron';
 import url from 'url';
 import * as actions from 'actions/widget';
+import { widgetInfoByIdSelector } from 'store/reducers/share/identification/selectors';
 import store from 'store/storeMain';
 import createWidget from 'main/utils/widget/createWidget';
 import updateWidgetContentBounds from 'main/utils/widget/updateWidgetContentBounds';
 import * as PATH from 'constants/path';
+import i18n from 'constants/i18n';
 
 const makeWidget = (id, info, isFocus) => {
   const widgetInfo = createWidget(id, info);
@@ -42,6 +44,32 @@ const makeWidget = (id, info, isFocus) => {
 
   widget.on('resize', () => {
     updateWidgetContentBounds(id, widget);
+  });
+
+  widget.on('close', (e) => {
+    const text = i18n().widget;
+    const targetInfo = widgetInfoByIdSelector(store.getState()).get(id);
+    const isMakeProgress = targetInfo.get('isMakeProgress');
+    const isEditProgress = targetInfo.get('isEditProgress');
+
+    /**
+     * Confirm when widget is making or is Editing.
+     */
+    if (isMakeProgress || isEditProgress) {
+      e.preventDefault();
+      const options = {
+        type: 'info',
+        title: text.closeWidget,
+        message: text.closeMessage(targetInfo.get('name')),
+        buttons: [text.ok, text.cancel],
+        icon: PATH.LOGO_ICON_PATH,
+      };
+      dialog.showMessageBox(options, (index) => {
+        if (index === 0) { // when click Yes button
+          widget.destroy();
+        }
+      });
+    }
   });
 
   widget.on('closed', () => {
