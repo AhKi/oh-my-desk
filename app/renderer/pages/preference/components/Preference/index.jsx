d2ad4bdd85fc44e8c01f2693e5d6885523cf0349
Hyunmoahn <mos_dev@naver.com>
2018-11-25 20:19:11 +0900

    Delete deprecated feature

--- app/renderer/pages/preference/components/Preference/index.jsx
+++ app/renderer/pages/preference/components/Preference/index.jsx
@@ -3,9 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import SettingContainer from 'renderer/pages/preference/containers/SettingContainer';
 import ModalContainer from 'renderer/components/Modal/ModalContainer';
 import UpdateContainer from 'renderer/pages/preference/containers/UpdateContainer';
-import HotKey from '../HotKey';
 import PreferenceHeader from '../PreferenceHeader';
-import Widget from '../Widget';
 import './Preference.scss';
 
 class Preference extends React.Component {
@@ -18,8 +16,6 @@ class Preference extends React.Component {
           <Switch>
             <Route exact path="/" component={SettingContainer} />
             <Route path="/update" component={UpdateContainer} />
-            <Route path="/hot-key" component={HotKey} />
-            <Route path="/widget" component={Widget} />
           </Switch>
         </div>
       </div>
