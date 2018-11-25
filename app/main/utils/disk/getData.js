347b4262b4832faab0625a9fa02b6524b87b578f
Hyunmoahn <mos_dev@naver.com>
2018-11-26 01:36:18 +0900

    Setting default widget information

--- app/main/utils/disk/getData.js
+++ app/main/utils/disk/getData.js
@@ -5,7 +5,7 @@ import * as SETTING from 'constants/setting';
 const getData = () => {
   const STORED_PATH = `${PATH.CONFIG_PATH}/${PATH.SETTING_FILE_NAME}`;
   if (!fs.existsSync(STORED_PATH)) {
-    return SETTING.defaultWidgets;
+    return JSON.parse(SETTING.defaultWidgets);
   }
 
   const storedData = fs.readFileSync(STORED_PATH, { encoding: 'utf-8' });
